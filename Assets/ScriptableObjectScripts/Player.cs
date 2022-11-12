9c90742cfee70a9317fc2bbb30439086f38b1dad
Joseph Y <jcy_y2000@hotmail.com>
2022-11-11 20:34:50 -0600

    Created Player and Partner Scriptable Object Scripts and put them into a ScriptableObjectScripts folder.

---
+++ Assets/ScriptableObjectScripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Player : ScriptableObject
+{
+    public string Name;
+    public float Charisma = 1f;
+    public float Intoxication = 1f;
+    public float Composure = 1f;
+    public float Skill = 1f;
+    public float Luck = 1f;
+}
