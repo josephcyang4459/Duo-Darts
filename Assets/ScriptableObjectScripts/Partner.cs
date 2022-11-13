25694d06357f51593f2cc8e77c827a4e80f43fb8
Joseph Y <jcy_y2000@hotmail.com>
2022-11-13 17:48:24 -0600

    Updated Partner Script so that Cutscenes relating to them are attached to the respective parters.

--- Assets/ScriptableObjectScripts/Partner.cs
+++ Assets/ScriptableObjectScripts/Partner.cs
@@ -12,13 +12,22 @@ public class Partner : ScriptableObject {
     public float Intoxication = 0f;
     public float Love = 0f;
     public RelatedCutScene[] RelatedCutScenes;
+    public CutScene DefaultCutScene;
+    public CutScene DefaultDrinkingCutScene;
     public Sprite[] Expressions;
     public int bias = -1;
+
+    public int CheckLoveCutScene() {
+        for(int i = 0; i < RelatedCutScenes.Length; i++)
+            if(!RelatedCutScenes[i].completed && Love >= RelatedCutScenes[i].loveRequirement)
+                return i;
+        return -1;
+    }
 }
 
 public class RelatedCutScene {
-    public Sprite Background;
-    string Location;
-    CutScene CutScene;
-    bool completed;
+    public string Location;
+    public CutScene CutScene;
+    public bool completed;
+    public int loveRequirement;
 }
